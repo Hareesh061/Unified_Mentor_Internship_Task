cab81092c2f948e34103148a88b5f6399fd8c9d6
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2024-07-20 10:46:52 +0530

    new commit

---
+++ weather_app/src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '770238a98d56fd1dacae91edc8a120bf';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
